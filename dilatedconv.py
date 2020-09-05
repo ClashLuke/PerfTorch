5c6b4d3d242e097e6f09800a68bfbe2275d9b16f
Luke <39779310+ClashLuke@users.noreply.github.com>
2020-09-05 13:36:06 +0200

    feat(dilatedconv): fuse unfold/bmm

--- dilatedconv.py
+++ dilatedconv.py
@@ -1,16 +1,21 @@
 import torch
 
 
-@torch.jit.script
-def flt(x: torch.Tensor, n):
-    return torch.logical_or(x >= n, x < 0)
+class DilatedConv(torch.jit.ScriptModule):
+    __constants__ = ['out_features']
 
+    def __init__(self, in_features, out_features, groups, size, kernel_size=3):
+        super().__init__()
+        if in_features % groups != 0:
+            raise UserWarning("Nope")
+        self.out_features = out_features
+        self.weight = torch.nn.Parameter(torch.empty(out_features, kernel_size ** 2 * in_features))
+        torch.nn.init.orthogonal_(self.weight)
 
-class Unfold(torch.jit.ScriptModule):
-    def __init__(self, groups, features, size):
-        super(Unfold, self).__init__()
-        feat = features
-        features //= groups
+        def flt(x: torch.Tensor, n):
+            return torch.logical_or(x >= n, x < 0)
+
+        features = in_features // groups
         with torch.no_grad():
             mask_kernel = torch.arange(-1, 2).view(-1, 1)
             pre_features = torch.cat([torch.tensor([2 ** i] * features) for i in range(groups)]).view(-1, 1, 1, 1, 1)
@@ -19,15 +24,16 @@ class Unfold(torch.jit.ScriptModule):
             theight = torch.tensor([size])
             hkernel = torch.arange(0, size).view(-1, 1).view(1, 1, 1, 1, -1)
             wkernel = torch.arange(0, size * size, size).view(-1, 1).view(1, 1, 1, -1, 1)
-            fkernel = torch.arange(0, size * size * feat, size * size).view(-1, 1, 1, 1, 1)
+            fkernel = torch.arange(0, size * size * in_features, size * size).view(-1, 1, 1, 1, 1)
             kernel = fkernel.add(wkernel).add(hkernel).add(pre_kernel).view(1, -1)
             height_mask = mask_kernel.mul(3).add(hkernel.view(1, -1, 1))
-            mask = torch.logical_or(flt(height_mask.add(mask_kernel * 2).view(feat, -1, 1), theight),
+            mask = torch.logical_or(flt(height_mask.add(mask_kernel * 2).view(in_features, -1, 1), theight),
                                     flt(mask_kernel.mul(3).add(hkernel.view(1, -1, 1)
-                                                               ).view(feat, -1, 1).transpose(1, 2),
+                                                               ).view(in_features, -1, 1).transpose(1, 2),
                                         theight)
                                     )
-            mask = mask.view(feat, size, 3, size, 3).transpose(1, -1).transpose(-2, -1).transpose(1, 2).reshape(1, -1)
+            mask = mask.view(in_features, size, 3, size, 3).transpose(1, -1).transpose(-2, -1).transpose(1, 2).reshape(
+                1, -1)
             kernel = torch.where(mask, torch.zeros(1, dtype=torch.long).expand_as(mask), kernel + 1)
         self.register_buffer('kernel', kernel)
 
@@ -35,27 +41,9 @@ class Unfold(torch.jit.ScriptModule):
         batch, feat, width, height = inp.size()
         output = torch.cat([torch.zeros((batch, 1), device=inp.device, dtype=inp.dtype), inp.view(batch, -1)], 1)
         output = output.gather(1, self.kernel.expand(batch, -1))
-        return output.view(batch, feat * 9, width * height)
-
-
-class DilatedConv(torch.jit.ScriptModule):
-    __constants__ = ['out_features']
-
-    def __init__(self, in_features, out_features, groups, size, kernel_size=3):
-        super().__init__()
-        if in_features % groups != 0:
-            raise UserWarning("Nope")
-        self.unfold = Unfold(groups, in_features, size)
-        self.out_features = out_features
-        self.weight = torch.nn.Parameter(torch.empty(out_features, kernel_size ** 2 * in_features))
-        torch.nn.init.orthogonal_(self.weight)
-
-    def forward(self, inp):
-        size = list(inp.size())
-        size[1] = self.out_features
-        data = self.unfold(inp)
-        data = self.weight.unsqueeze(0).expand(size[0], -1, -1).bmm(data)
-        data = data.view(*size)
+        data = output.view(batch, feat * 9, width * height)
+        data = self.weight.unsqueeze(0).expand(batch, -1, -1).bmm(data)
+        data = data.view(batch, self.weight.size(0), width, height)
         return data
 
 
